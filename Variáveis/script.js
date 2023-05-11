ed031e70980424f02cc0169bbb8249fe204a240a
Astheum <119074740+Astheum@users.noreply.github.com>
2023-05-11 07:24:26 -0300

    Variáveis

---
+++ Variáveis/script.js
@@ -0,0 +1,64 @@
+// /*  Exemplo 01 */
+// var nome = 'Matheus'
+// var idade = 18
+// var possuiFaculdade = true
+// console.log(nome, idade, possuiFaculdade)
+
+// /* Exemplo 02 */
+// var preco = 20
+// var totalComprado = 5
+// var valorTotal = preco * totalComprado
+// console.log(valorTotal)
+
+// /* Exemplo 03 */
+// var sobrenome = 'Matheus', 
+//       cidade = 'Patos de Minas'
+// console.log(sobrenome, cidade);
+
+// /* Exemplo 04 */
+// var semDefinir
+// console.log(semDefinir)
+
+// /* Exemplo 05 (ERRO)
+// console.log(aindaNaoDefinido); */
+
+// /* Exemplo 06 */
+// var comida
+// console.log(comida)
+// comida = 'Pizza'
+// console.log(comida)
+
+// /* Exemplo 07 */
+// var time = 'Vasco'
+// time = 'Flamengo'
+// console.log(time) 
+
+// // let não pode ser usado novamente
+// // const não pode ser redefinido
+
+/* >>> Exercicios <<< */
+
+// Declarar uma variavel com o seu nome
+var nome = 'Matheus'
+console.log(nome)
+
+// Declarar uma variavel com a sua idade
+var idade = 18
+console.log(idade)
+
+// Declarar uma variavel com sua comida favorita e não atribuir valor
+var comidaFav
+console.log(comidaFav)
+
+// Atribuir valor a sua comida fovorita
+comidaFav = 'Sanduiche'
+console.log(comidaFav)
+
+// Declarar 5 variáveis diferentes sem valores
+var time, 
+      faculdade, 
+      pais,  
+      endereco, 
+      profissao
+
+console.log(time, faculdade, pais, endereco, profissao)
