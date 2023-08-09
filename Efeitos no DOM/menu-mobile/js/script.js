521c061c590545ba49ef2fdec469c191c9a2de70
Astheum <119074740+Astheum@users.noreply.github.com>
2023-08-09 16:09:06 -0300

    .

---
+++ Efeitos no DOM/menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import  initSmoothScroll from "./modules/smooth-scroll.js"
+import  initScrollAnimation from "./modules/scroll-animation.js"
+import  initAccordion from "./modules/accordion-list.js"
+import  initTabNav from "./modules/nav-menu.js"
+import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
+import initDropdownMenu from "./modules/dropdown-menu.js"
+import initMenuMobile from "./modules/menu-mobile.js"
+
+initSmoothScroll()
+initScrollAnimation()
+initAccordion()
+initTabNav()
+initModal()
+initTooltip()
+initDropdownMenu()
+initMenuMobile()
