3e62008133f96084971003c39d2ee518a343c3a6
Astheum <119074740+Astheum@users.noreply.github.com>
2023-08-10 14:50:51 -0300

    .

---
+++ Efeitos no DOM/anima-numeros/js/script.js
@@ -0,0 +1,20 @@
+import  initSmoothScroll from "./modules/smooth-scroll.js"
+import  initScrollAnimation from "./modules/scroll-animation.js"
+import  initAccordion from "./modules/accordion-list.js"
+import  initTabNav from "./modules/nav-menu.js"
+import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
+import initDropdownMenu from "./modules/dropdown-menu.js"
+import initMenuMobile from "./modules/menu-mobile.js"
+import initAnimaNumeros from "./modules/anima-numeros.js"
+
+
+initSmoothScroll()
+initScrollAnimation()
+initAccordion()
+initTabNav()
+initModal()
+initTooltip()
+initDropdownMenu()
+initMenuMobile()
+initAnimaNumeros()
